67875c3d8d61741204ed889b8d005f391031337a
Mike Westbom <totallymike@gmal.com>
2010-01-14 03:04:15 -0500

    Bound url commands

--- lastbot.tcl
+++ lastbot.tcl
@@ -341,3 +341,5 @@ bind msg $last(who) $last(char)list msg_list
 bind pub $last(who) $last(char)compare compare
 bind pub $last(who) $last(char)cp compare
 bind pub $last(who) $last(char)reg pub_register
+bind msg $last(who) $last(char)url msg_url
+bind pub $last(who) $last(char)url pub_url
